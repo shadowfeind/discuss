5d2dddd20bb1570205ecba2db32855496fa02b78
shadowfeind <travestroy007@gmail.com>
2024-04-09 11:13:06 +0545

    making routes and route helper

---
+++ app/topics/[slug]/page.tsx
@@ -0,0 +1,5 @@
+const TopicShow = () => {
+  return <div>TopicShow</div>;
+};
+
+export default TopicShow;
---
+++ app/topics/[slug]/posts/new/[postId]/page.tsx
@@ -0,0 +1,5 @@
+const ShowSinglePost = () => {
+  return <div>ShowSinglePost</div>;
+};
+
+export default ShowSinglePost;
